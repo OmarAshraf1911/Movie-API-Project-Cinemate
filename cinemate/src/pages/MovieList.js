63b7cb7689c4842b804b0c5605e6d6e453011f22
OmarAshraf1911 <omarashraf.90.5.370@gmail.com>
2024-07-10 00:16:06 +0300

    Upload files

--- cinemate/src/pages/MovieList.js
+++ cinemate/src/pages/MovieList.js
@@ -1,3 +1,15 @@
+import { Card } from "../components";
 export const MovieList = () => {
-  return <main>MovieList</main>;
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
+    </main>
+  );
 };
